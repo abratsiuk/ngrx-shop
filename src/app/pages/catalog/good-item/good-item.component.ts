04fe8a6bc49e2eb410a1f980ba28c651295cd654
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2025-06-03 22:00:34 +0300

    fix classes in good-list and good-item

--- src/app/pages/catalog/good-item/good-item.component.ts
+++ src/app/pages/catalog/good-item/good-item.component.ts
@@ -1,12 +1,15 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Input, OnInit, ViewEncapsulation} from '@angular/core';
 import {IGoodItem} from '../../../../interfaces/IGoodItem';
 
 @Component({
   selector: 'app-good-item',
   templateUrl: './good-item.component.html',
-  styleUrls: ['./good-item.component.scss']
+  styleUrls: ['./good-item.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GoodItemComponent implements OnInit {
+  @HostBinding('class') hostClasses = 'GoodItem';
+
   @Input() good!: IGoodItem;
 
   constructor() {
--- src/app/pages/catalog/goods-list/goods-list.component.ts
+++ src/app/pages/catalog/goods-list/goods-list.component.ts
@@ -1,13 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit, ViewEncapsulation} from '@angular/core';
 import {IGoodItem} from '../../../../interfaces/IGoodItem';
 import {itemsListMock} from '../../../../mock/mock_items_list';
 
 @Component({
   selector: 'app-goods-list',
   templateUrl: './goods-list.component.html',
-  styleUrls: ['./goods-list.component.scss']
+  styleUrls: ['./goods-list.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class GoodsListComponent implements OnInit {
+  @HostBinding('class') hostClasses = 'GoodsList';
+
   goods: IGoodItem[] = [];
 
   constructor() {
